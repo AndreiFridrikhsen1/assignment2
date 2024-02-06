cb55f9917f3951290052cff5fcf819e4e845c5fb
AndreiFridrikhsen1 <152751636+AndreiFridrikhsen1@users.noreply.github.com>
2024-02-05 21:19:24 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,146 @@
+<?php
+echo "<script>alert('Hover over the lyrics to see the changes')</script>";
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Assignment2</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Agbalumo&display=swap" rel="stylesheet">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poor+Story&display=swap" rel="stylesheet">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Protest+Revolution&display=swap" rel="stylesheet">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Caveat:wght@400..700&family=Protest+Revolution&display=swap" rel="stylesheet">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Allerta+Stencil&display=swap" rel="stylesheet">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Shadows+Into+Light&display=swap" rel="stylesheet">
+
+    <link rel="icon" type="image/x-icon" href="images/favicon.png">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Bangers&display=swap" rel="stylesheet">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Londrina+Solid&display=swap" rel="stylesheet">
+
+</head>
+<body>
+    <header>
+        <h1>What The Font</h1>
+
+    </header>
+    <section class="main">
+        <section>
+        <h2>Verse 1</h2>
+            <div>            
+                <p>You know I have to go</p>
+                <p>Back to the place I hate the most</p>
+                <p>Long ago, it used to help me grow</p>
+                <p>Always kept me feeling safe from coast to coast</p>
+            </div>            
+        </section>
+
+        <section>
+        <h2>Verse 2</h2>
+            <div>
+                
+                <p>Lock the gates at night and don’t set my place</p>
+                <p>I won’t be back to see how the snow covers leaves</p>
+                <p>I counted down the hours until I found a shortcut home to you</p>
+                <p class="calm">But I’m just passing through</p>
+                <p class="calm">Barely a guest, a stranger in my own home</p>
+            </div>
+        </section>
+
+        <section>
+        <h2>Chorus</h2>
+            <div>
+                
+                <p>I'm back in Toronto, but just 'til tomorrow</p>
+                <p>That's when the bleeding stops again</p>
+                <p>Put off all the feelings, forget all the meanings</p>
+                <p>Just let it cycle through my head</p>
+                <p>We’re spinning again, the sinning is innocent</p>
+                <p class="calm">'Til we realize that we're back in Toronto once again</p>
+            </div>
+        </section>
+
+        <section>
+        <h2>Verse 3</h2>
+            <div>            
+                
+                <p>You kicked and screamed until you met your defeat</p>
+                <p class="calm">And slammed the door behind me, where you stood for weeks</p>
+                <p>I spent all night pretending I was alright</p>
+                <p class="calm">I justified, I planned, but I know deep inside</p>
+                <p>I can’t withstand temptation by your hand</p>
+            </div>
+        </section>
+        <section>
+        <h2>Chorus</h2>
+            <div>
+                
+                <p>I'm back in Toronto, but just 'til tomorrow</p>
+                <p>That's when the bleeding stops again</p>
+                <p>Put off all the feelings, forget all the meanings</p>
+                <p>Just let it cycle through my head</p>
+                <p>We’re spinning again, the sinning is innocent</p>
+                <p>'Til we realize that we're back in Toronto once again</p>
+            </div>
+        </section>
+        <section class="bridge">
+        <h2>Bridge</h2>
+            <div>
+                <p class="calm">Back home, Liberty</p>
+                <p class="calm">Cutting through the park, I see</p>
+                <p>Everything has changed except for me</p>
+                <p>This place is called home</p>
+                <p>But I’ve never been so scared</p>
+                <p class="calm">To face the cold, breathe in the truth from the air</p>
+                <p>This place is called home</p>
+                <p>But from Kensington to Dee’s</p>
+                <p class="calm">Everything I pass, it plants a seed</p>
+                <p>And it takes me back to a time when all I had</p>
+                <p>Was a cloud over my head, weights on my feet</p>
+                <p>This place is called home</p>
+                <p class="calm">But it’s not where I will stay, it’s all temporary</p>
+                <p>I know I’ll never move away</p>
+                <p>But I know this city will always feel strange</p>
+                <p class="calm">This city is a stranger to me</p>
+            </div>
+        </section>
+
+  
+
+    </section>
+    <footer>
+        <h3>Artist:  <span> Silverstein </span></h3>
+        <h3>Name of the song: <span> Toronto </span></h3>
+        
+        <a href="https://www.flaticon.com/free-icons/maple-leaf" title="maple leaf icons">Maple leaf icons created by Aldo Cervantes - Flaticon</a>
+        <a href="https://www.flaticon.com/free-icons/fire" title="fire icons">Fire icons created by iconixar - Flaticon</a>
+        
+    </footer>
+</body>
+</html>
